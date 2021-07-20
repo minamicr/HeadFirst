b53225c87332d2112d9aa3e730f4539dee082f39
CrisMinami <cristina.minami@bradesco.com.br>
2021-07-20 06:58:13 -0300

    Implemented java observer pattern

---
+++ src/main/java/com/observer/java/pattern/ForecastDisplay.java
@@ -0,0 +1,33 @@
+package com.observer.java.pattern;
+
+import com.observer.pattern.model.DisplayElement;
+import java.util.Observable;
+import java.util.Observer;
+
+public class ForecastDisplay implements Observer, DisplayElement {
+
+  private float currentPressure = 29.2f;
+  private float lastPressure;
+
+  public ForecastDisplay(Observable observable){
+    observable.addObserver(this);
+  }
+
+  @Override
+  public void update(Observable obs, Object arg) {
+    WeatherData weatherData;
+    if (obs instanceof WeatherData){
+      weatherData = (WeatherData)obs;
+      lastPressure = currentPressure;
+      currentPressure = weatherData.getPressure();
+    }
+
+    display();
+
+  }
+
+  @Override
+  public void display(){
+    System.out.println("Pressure changes from " + lastPressure + " to " + currentPressure);
+  }
+}
